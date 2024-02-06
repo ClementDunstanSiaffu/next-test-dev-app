15c74d645f414138de02ab1543fdd3fe5d0eee4f
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2024-02-07 01:03:59 +0300

    finalize all components

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,15 @@ import React from 'react';
 import Products from '../components/ProductContainer/Products';
 import Category from '../components/Category/Category';
 import Advertsiment from '../components/Advertsiment/Advertsiment';
+import SearchContainer from '../components/SearchContainer/SearchContainer';
 
 export default function Page() {  
   return(
     <>
-        <Advertsiment />
-        <Category />
-        <Products />
+      <SearchContainer />
+      <Advertsiment />
+      <Category />
+      <Products />
     </>
   )
 }
---
+++ components/SearchContainer/SearchContainer.tsx
@@ -0,0 +1,32 @@
+
+"use client"
+
+import React from 'react';
+import './assets/styles/SearchContainer.css'
+import { TbArrowBigDownLine } from "react-icons/tb";
+import { RxThickArrowDown } from "react-icons/rx";
+import { FaSearch } from "react-icons/fa";
+
+export default function SearchContainer(){
+
+    return(
+        <div className='search-outer-container'>
+            <div className='logo-container'>
+                <span className='bold-high-text'>TextValley</span>
+                <span className='light-low-text'>연말 결산_디지털</span>
+            </div>
+            <div className='search-input-container'>
+                <span className='search-icon-container'><FaSearch color='grey' size={14} /></span>
+                <input 
+                    type='text' 
+                    className='input-contents-container' 
+                    placeholder='연말결산_디지털연말결산_디지털'
+                />
+            </div>
+            <div className='icon-container'>
+                <span><RxThickArrowDown /></span>
+                연말결산_디지털
+            </div>
+        </div>
+    )
+}
